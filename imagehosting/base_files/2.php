c22e2b8035f02d60d17932ae59ac84ee5c58022d
Vikas <robolab1478@gmail.com>
2014-04-10 08:28:34 -0500

    first commit

---
+++ imagehosting/base_files/2.php
@@ -0,0 +1,2 @@
+" data-lightbox="example-1">
+<img class="example-image" src="upload/
---
+++ imagehosting/base_files/3.php
@@ -0,0 +1,11 @@
+" alt="thumb-1"/>
+		</a></center>
+			</div>
+			<br />
+			<a id="scrollUp" href="#top" style="position: fixed; z-index: 1001;">&#8593;</a>
+			<div class="<?php if($footer == "big") echo"footer";else {if($footer=="small") echo "baby_footer"; else echo"hidden";} ?>">
+				content goes here.
+			</div>
+		</div>
+	</body>
+</html>
