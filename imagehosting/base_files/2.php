69f39dd3df2098b6549fe02de9e18f98fb698e60
Vikas <robolab1478@gmail.com>
2014-08-21 17:55:49 -0500

    Removal of ImageHosting (not necessary)
    Ridding ImageHosting since it's the source.

--- imagehosting/base_files/2.php
+++
@@ -1,2 +0,0 @@
-" data-lightbox="example-1">
-<img class="example-image" src="upload/
--- imagehosting/upload/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$title = "Random Page";
-$stuff ="<b>nothing to show you here :)</b>";
-include('nav.php');
-exit();
-?>
